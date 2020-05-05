2f11595879c5c7eeba4a9ce8bc79bbfe32c2e06a
SanneSchroduer <s.schroduer@hotmail.com>
2020-05-05 21:56:20 +0200

    Added list of steps for starting mySQL container

--- convertVCF.py
+++ convertVCF.py
@@ -11,4 +11,23 @@ for record in vcf_reader:
           record.INFO['AF']
           )
 
+##  DATABASE
+# downloaden mysql server docker image
+#       docker pull mysql/mysql-server:tag
+#       naam  van de image is "mysql/mysql-server:tag"
+#       tag zorgt ervoor dat de latest tag gebruikt wordt
 
+# starten van een msql server instance (container)
+#       docker run --name=mysql1 -d mysql/mysql-server:tag
+#       --name geeft de naam aan je container
+#       -d zorgt ervoor dat de container op de achtergrond blijft runnen
+
+# controleren of container runt
+#       docker ps
+# connecten aan de mysql server vanuit de container
+#       docker exec -it mysql1 mysql -uroot -p
+
+
+
+# mysql database maken in een eigen Docker container
+# mysql connector aanmaken, met een insert de data opslaan in een database
