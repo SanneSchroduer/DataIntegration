dcb5aa3f7884d793a154562f3de9f789f63516dc
SanneSchroduer <s.schroduer@hotmail.com>
2020-05-26 10:15:32 +0200

    Added table to result page and added css

--- parser.py
+++ parser.py
@@ -8,21 +8,29 @@ def is_allowed(filename):
     return '.' in filename and \
            filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
-
 def get_result(filename):
-
-    with open(filename, 'r') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter='\t')
+    result = []
+    filepath = 'inbox/'+filename
+    with open(filepath, 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
-            chrom = row[0]
-            pos = row[1]
-            id = row[3]
-            ref = row[4]
-            alt = row[5]
-            prob = row[10]
+            try:
+                print(row)
+                chrom = row[0]
+                pos = row[1]
+                id = row[2]
+                ref = row[3]
+                alt = row[4]
+                prob = row[10].split('=')[1]
+                variant = [chrom, pos, id, ref, alt, prob]
+                result.append(variant)
+            except IndexError:
+                pass
     #logic for comparing the input data against the database
     #parsing the input file; extracting the chromosome, the postition, the nucleotide and the variant.
     #possibly the probability score
-    return filename
 
-get_result(filename)
+    return result
+
+# filename = 'test.csv'
+# get_result(filename)
