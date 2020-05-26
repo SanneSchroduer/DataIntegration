6e6adc93a0b12e2de4aad12d3e0baa5d8df39ead
SanneSchroduer <s.schroduer@hotmail.com>
2020-05-26 13:51:10 +0200

    Added sticky table header

--- parser.py
+++ parser.py
@@ -10,18 +10,22 @@ def is_allowed(filename):
 
 def get_result(filename):
     result = []
+    pos_list = []
     filepath = 'inbox/'+filename
     with open(filepath, 'r') as csv_file:
         csv_reader = csv.reader(csv_file, delimiter=',')
         for row in csv_reader:
             try:
-                print(row)
                 chrom = row[0]
                 pos = row[1]
                 id = row[2]
                 ref = row[3]
                 alt = row[4]
                 prob = row[10].split('=')[1]
+
+                if pos in pos_list:
+                    print(pos)
+                pos_list.append(pos)
                 variant = [chrom, pos, id, ref, alt, prob]
                 result.append(variant)
             except IndexError:
@@ -34,3 +38,22 @@ def get_result(filename):
 
 # filename = 'test.csv'
 # get_result(filename)
+
+
+'''
+filename get_variants.py 
+e.g. chr18 get_variants.py
+
+get_variants.py: 
+- draait lokaal
+- leest input file in
+- stuurt request naar API (draait vanuit een docker container op 127.0.0.1:5000) met filenaam van input file
+- schrijft alle malign variants naar output file in directory van 
+
+output: chr18_malign
+'''
+#
+# for x in files:
+#     request(127.0.0.1:500, 'chr18')
+
+
